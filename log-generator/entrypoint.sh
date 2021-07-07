acccc4fbfe2e38148fff6e469d76073ec897afc8
jkosik <juraj.kosik@gmail.com>
2021-07-07 21:32:25 +0200

    sec

--- log-generator/entrypoint.sh
+++ log-generator/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-  echo "Random string $RANDOM printed to stdout."
+  SEC_ELAPSED=$(date +%s%N)
+  echo "Random string $SEC_ELAPSED (seconds elapsed) printed to stdout."
   sleep 3
 done
