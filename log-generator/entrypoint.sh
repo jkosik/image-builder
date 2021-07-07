a7206a533af247b45d297cc556b5c1da706630eb
jkosik <juraj.kosik@gmail.com>
2021-07-07 21:25:58 +0200

    log-gen 0.0.1

--- log-generator/entrypoint.sh
+++ log-generator/entrypoint.sh
@@ -3,6 +3,5 @@
 while true; do
   RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   echo "Random string $RANDOM printed to stdout."
-  logger -i "Random string $RANDOM printed to syslog."
   sleep 3
 done
