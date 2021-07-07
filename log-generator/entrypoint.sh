d8c0009fefe4c4ca9ae5dd6c192febb23e6032e7
jkosik <juraj.kosik@gmail.com>
2021-07-07 21:23:07 +0200

    log-gen 0.0.1

--- log-generator/entrypoint.sh
+++ log-generator/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while true; do
-  RANDOM=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 10 ; echo '')
+  RANDOM=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
   echo "Random string $RANDOM printed to stdout."
   logger -i "Random string $RANDOM printed to syslog."
   sleep 3
