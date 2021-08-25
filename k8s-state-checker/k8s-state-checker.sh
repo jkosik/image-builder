0bd8d58fda433837fa0c4b6748e9c9829ade68fd
Juraj Kosik <juraj.kosik@gmail.com>
2021-08-25 14:00:43 +0200

    k8s state

--- k8s-state-checker/k8s-state-checker.sh
+++ k8s-state-checker/k8s-state-checker.sh
@@ -3,8 +3,9 @@
 kubectl get po -l app!=k8s-state-checker --no-headers > pods
 helm list > helm-charts
 
-
 kubectl create cm expected-versions --from-file=pods --from-file=charts
+sleep 5
+
 kubectl describe cm expected-versions
 
 echo "=== MD5 Hash of the expected-version ConfigMap (kubectl get cm expected-versions -o yaml) ==="
